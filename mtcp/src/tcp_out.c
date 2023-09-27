e2dc69cedc49b5c9fd47efd60ccbe89cb658c64f
Haathim-Vagrant-mTCP-3 <haathimmunas@gmail.com>
2023-09-27 08:10:38 +0000

    Update

--- mtcp/src/tcp_out.c
+++ mtcp/src/tcp_out.c
@@ -712,6 +712,7 @@ SendTCPPacket(struct mtcp_manager *mtcp, tcp_stream *cur_stream,
 
 	if (payloadlen + optlen > cur_stream->sndvar->mss) {
 		TRACE_ERROR("Payload size exceeds MSS\n");
+		sleep(5);
 		return ERROR;
 	}
 
@@ -1044,7 +1045,8 @@ FlushTCPSendingBuffer(mtcp_manager_t mtcp, tcp_stream *cur_stream, uint32_t cur_
 		/* payload size limited by remaining window space */
 		len = MIN(len, remaining_window);
 		/* payload size limited by TCP MSS */
-		pkt_len = MIN(len, sndvar->mss - CalculateOptionLength(TCP_FLAG_ACK));
+		// pkt_len = MIN(len, sndvar->mss - CalculateOptionLength(TCP_FLAG_ACK));
+		pkt_len = MIN(len, sndvar->mss - CalculateOptionLengthMPTCP(TCP_FLAG_ACK, MPTCP_OPTION_CAPABLE, len));
 
 #if RATE_LIMIT_ENABLED
 		// update rate
